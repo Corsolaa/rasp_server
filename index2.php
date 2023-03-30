0438af9da17dfaee7d16e88b2fd0df97dc93b57b
John <bruno.bouwman4@gmail.com>
2023-03-30 22:16:37 +0200

    start
    Made some ideas

---
+++ index2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Archive server</title>
+    <link rel="icon" href="./favicon.ico">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style_2.0.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./fontAwesome.js" crossorigin="anonymous"></script>
+    <!-- Google font -->
+    <!--    <link rel="preconnect" href="https://fonts.googleapis.com">-->
+    <!--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
+    <!--    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;-->
+    <!--                    1,700&display=swap" rel="stylesheet">-->
+</head>
+<body>
+<header class="header">
+    <h1> This is my archive </h1>
+    <p> please respect the websites that are displayed here </p>
+</header>
+<div class="content">
+    <div class="phone">
+        <div class="app">
+            <div class="icon">
+                <img src="./images/tic_toc_logo.png" alt="TicToc">
+
+            </div>
+            <div class="text">
+                Tic Toc
+            </div>
+        </div>
+        make it screen with apps
+    </div>
+</div>
